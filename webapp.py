8079b478b2566c8f4c780591a6e9a19a9b412da9
clarissa <clarissa.gasiciel@gmail.com>
2019-06-02 20:23:59 -0500

    Added apt to form and added to webapp.py

--- webapp.py
+++ webapp.py
@@ -6,5 +6,24 @@ from db_connector.db_connector import connect_to_database, execute_query
 webapp = Flask(_name_)
 
 # Route for adding a new voter
-@webapp.route('/add_new_voter')
+@webapp.route('/add_new_voter', methods=['POST']['GET')
 # Provide a view which responds to any requests on this productVendor
+def add_new_voter():
+    db_connection = connect_to_database()
+    if request.method == 'POST':
+        print("Adding new voter");
+        usrname = request.form["username"]
+        pwd = request.form["pwd"]
+        confPwd = request.form["confPwd"]
+        fname = request.form["fname"]
+        lname = request.form["lname"]
+        ssn = request.form["ssn"]
+        dob = request.form["dob"]
+        addr = request.form["stAddr"]
+        apt = request.form["apt"]
+        city = request.form["city"]
+        state = request.form["state"]
+        zip = request.form["zip"]
+        email = request.form["email"]
+
+    query = 'INSERT INTO user_info (username, pswd_hash, first_name, last_name, birthday, street, apt)'
